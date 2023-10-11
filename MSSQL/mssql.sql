23e48c10048700a380150fe00b19691f77cd3835
lukaskierzek <lukas.kierzek@gmail.com>
2023-10-11 20:51:47 +0200

    Update mssql.sql
    Refactor procedure

--- MSSQL/mssql.sql
+++ MSSQL/mssql.sql
@@ -30,34 +30,34 @@ GO
 -- Return a category name with the number of products -----------------------------------------------------------------
 -----------------------------------------------------------------------------------------------------------------------
 
-CREATE PROCEDURE CategoriesWithCount
+CREATE PROCEDURE CategoriesWithProductCount
 AS
 BEGIN
     SET NOCOUNT ON;
-    WITH cte_categories_with_cout (CategoryName, CategoryCount) AS (
-        SELECT c.category_name, count(*) CategoryCount
+    WITH cte_categories_with_cout (CategoryName, ProductCount) AS (
+        SELECT c.category_name, count(*) ProductCount
         FROM BikeStores.production.products p
         INNER JOIN BikeStores.production.categories c ON c.category_id = p.category_id
         GROUP BY c.category_name
     )
     SELECT *
     FROM cte_categories_with_cout
-    ORDER BY CategoryCount DESC;
+    ORDER BY ProductCount DESC;
 END
 GO
 
-EXEC CategoriesWithCount
+EXEC CategoriesWithProductCount
 GO;
 
 -- Result:
--- +-------------------+-------------+
--- |CategoryName       |CategoryCount|
--- +-------------------+-------------+
--- |Cruisers Bicycles  |78           |
--- |Mountain Bikes     |60           |
--- |Road Bikes         |60           |
--- |Children Bicycles  |59           |
--- |Comfort Bicycles   |30           |
--- |Electric Bikes     |24           |
--- |Cyclocross Bicycles|10           |
--- +-------------------+-------------+
+-- +-------------------+------------+
+-- |CategoryName       |ProductCount|
+-- +-------------------+------------+
+-- |Cruisers Bicycles  |78          |
+-- |Mountain Bikes     |60          |
+-- |Road Bikes         |60          |
+-- |Children Bicycles  |59          |
+-- |Comfort Bicycles   |30          |
+-- |Electric Bikes     |24          |
+-- |Cyclocross Bicycles|10          |
+-- +-------------------+------------+

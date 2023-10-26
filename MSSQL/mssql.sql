6f8fb0bda515a9179b95884329a0253876d4b5dc
lukaskierzek <lukas.kierzek@gmail.com>
2023-10-26 15:23:38 +0200

    Update mssql.sql
    Add View

--- MSSQL/mssql.sql
+++ MSSQL/mssql.sql
@@ -61,3 +61,44 @@ GO;
 -- |Electric Bikes     |24          |
 -- |Cyclocross Bicycles|10          |
 -- +-------------------+------------+
+
+-----------------------------------------------------------------------------------------------------------------------
+-- View with amount of order_status in particular year, store and state -----------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------
+
+CREATE OR ALTER VIEW sales.amount_order_status
+AS
+SELECT "order_status" = CASE
+    WHEN order_status = 1 THEN 'Pending'
+    WHEN order_status = 2 THEN 'Processing'
+    WHEN order_status = 3 THEN 'Rejected'
+    WHEN order_status = 4 THEN 'Completed'
+    END,
+    year(o.order_date) year,
+    count(o.order_status) amount,
+    s.store_name,
+    s.state
+FROM BikeStores.sales.orders o
+         INNER JOIN BikeStores.sales.stores s ON o.store_id = s.store_id
+GROUP BY order_status, year(o.order_date), s.store_name, s.state;
+GO
+
+SELECT * FROM sales.amount_order_status
+WHERE state = 'TX'
+ORDER BY year DESC,
+         amount;
+GO;
+
+-- Result:
+-- +------------+----+------+-------------+-----+
+-- |order_status|year|amount|store_name   |state|
+-- +------------+----+------+-------------+-----+
+-- |Rejected    |2018|4     |Rowlett Bikes|TX   |
+-- |Processing  |2018|5     |Rowlett Bikes|TX   |
+-- |Pending     |2018|8     |Rowlett Bikes|TX   |
+-- |Completed   |2018|17    |Rowlett Bikes|TX   |
+-- |Rejected    |2017|8     |Rowlett Bikes|TX   |
+-- |Completed   |2017|67    |Rowlett Bikes|TX   |
+-- |Rejected    |2016|7     |Rowlett Bikes|TX   |
+-- |Completed   |2016|58    |Rowlett Bikes|TX   |
+-- +------------+----+------+-------------+-----+

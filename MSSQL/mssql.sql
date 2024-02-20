7c4bebc5813ed9af3cd4ce9240946a277b5c5416
Łukasz Kierzek <39835435+lukaskierzek@users.noreply.github.com>
2024-02-20 14:06:20 +0100

    Add new query

--- MSSQL/mssql.sql
+++ MSSQL/mssql.sql
@@ -93,7 +93,7 @@ SELECT order_status
     , state
     FROM sales.amount_order_status
 WHERE state = 'TX'
-group by year
+GROUP BY year
        , order_status
        , store_name
        , state
@@ -142,3 +142,22 @@ GO;
 -- +-----------+----------+---------+-----------+------------------ +--------+-----+-----+--------+
 -- |1447       |John      |Connor   |111-222-333|john.connor@jc.com |123 grov|Sandy|OR   |97055   |
 -- +-----------+----------+---------+-----------+------------------ +--------+-----+-----+--------+
+
+-- ===================================================================================================================
+-- List of products whose average list_price is greater than or equal to the average list for the category.
+-- ===================================================================================================================
+
+WITH cte_avg_list_price_by_category (categoryId, avgForCategory) AS (
+    SELECT category_id
+          ,avg(list_price)
+    FROM BikeStores.production.products
+    GROUP BY category_id
+) SELECT p.product_name
+        ,c.category_name
+        ,p.list_price
+        ,cte.avgForCategory
+  FROM production.products p
+      INNER JOIN cte_avg_list_price_by_category cte ON cte.categoryId = p.category_id
+      INNER JOIN production.categories c ON c.category_id = p.category_id
+  WHERE p.list_price >= cte.avgForCategory
+  ORDER BY category_name, list_price DESC;
